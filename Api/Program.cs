97eb53cf8da13e0db441b1037fc34f53b38cc548
Hubert Mikołajewicz <hubmik@hotmail.com>
2024-06-24 09:36:05 +0200

    Change way of getting path from configuration

--- Api/Program.cs
+++ Api/Program.cs
@@ -6,13 +6,13 @@ using Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 // Add configuration
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-
+var csvFileDirectory = Path.GetFullPath(@"..\");
 // Get the CSV file path from configuration
 string csvFilePath = builder.Configuration.GetValue<string>("CsvFilePath");
 // Add services to the container.
-
+var fullCsvPath = csvFileDirectory + csvFilePath;
 builder.Services.AddControllers();
-builder.Services.AddScoped<IDeviceRepository>(provider => new DeviceRepository(csvFilePath));
+builder.Services.AddScoped<IDeviceRepository>(provider => new DeviceRepository(fullCsvPath));
 builder.Services.AddScoped<IDeviceService, DeviceService>();
 builder.Services.AddCors(options =>
 {
--- Cli/Program.cs
+++ Cli/Program.cs
@@ -8,9 +8,11 @@ class Program
         var builder = new ConfigurationBuilder()
                 .AddJsonFile($"appsettings.json", true, true);
         var config = builder.Build();
+        var csvFileDirectory = Path.GetFullPath(@"..\..\..\..\");
         string csvFilePath = config["CsvFilePath"];
+        var fullCsvPath = csvFileDirectory + csvFilePath;
 
-        DeviceRepository cameraRepository = new DeviceRepository(csvFilePath);
+        DeviceRepository cameraRepository = new DeviceRepository(fullCsvPath);
         cameraRepository.GetDevicesByName(args[0]).ForEach(i=> Console.WriteLine("{0} | {1} | {2} | {3}\t", i.Number, i.FullName, i.Latitude, i.Longitude));
     }
 }
