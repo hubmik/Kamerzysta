772133d0cf8436343c2c00d19746a4844879c193
Hubert Mikołajewicz <hubmik@hotmail.com>
2024-06-24 09:46:23 +0200

    Fix path

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -8,11 +8,12 @@ class Program
         var builder = new ConfigurationBuilder()
                 .AddJsonFile($"appsettings.json", true, true);
         var config = builder.Build();
-        var csvFileDirectory = Path.GetFullPath(@"..\..\..\..\");
+        var csvFileDirectory = Path.GetFullPath(@"..\");
         string csvFilePath = config["CsvFilePath"];
         var fullCsvPath = csvFileDirectory + csvFilePath;
-
         DeviceRepository cameraRepository = new DeviceRepository(fullCsvPath);
-        cameraRepository.GetDevicesByName(args[0]).ForEach(i=> Console.WriteLine("{0} | {1} | {2} | {3}\t", i.Number, i.FullName, i.Latitude, i.Longitude));
+
+        string name = args[0];
+        cameraRepository.GetDevicesByName(name).ForEach(i=> Console.WriteLine("{0} | {1} | {2} | {3}\t", i.Number, i.FullName, i.Latitude, i.Longitude));
     }
 }
